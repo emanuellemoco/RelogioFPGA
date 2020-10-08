c97dce81c249d831b76f78a4d63d4565ea13299f
Emanuelle <emanuellesmoco@gmail.com>
2020-10-08 10:31:15 -0300

    Todos os perifericos funcionando
    Co-authored-by: Junior <leonardom1808@gmail.com>
    Co-authored-by: Lucas <lucaslealvale01@gmail.com>

--- memoriaROM.vhd
+++ memoriaROM.vhd
@@ -29,6 +29,16 @@ end entity;
 --   RAM            OK
 --   Base tempo
 
+
+  -- -------------Endereçamento------------
+  -- BASE TEMPO ------------------------[0]
+  -- HEX -----------------------------[1~6]
+  -- LED ------------------------------[7]
+  -- SW  ------------------------------[8]
+  -- KEY ------------------------------[9]
+  -- RAM -----------------------------[10~63]
+
+
 architecture assincrona of memoriaROM is
 
     type blocoMemoria is array(0 to 2 ** addrWidth - 1) of std_logic_vector(dataWidth - 1 downto 0);
@@ -37,16 +47,47 @@ architecture assincrona of memoriaROM is
         return blocoMemoria is variable tmp : blocoMemoria := (others => (others => '0'));
     begin
 
-        --        OPCODE  REGA  REGB  REGC    IMED/EndPerf
-        tmp(0) := leaw & NOP & NOP & R01 & b"0000000011";
-        tmp(1) := leaw & NOP & NOP & R02 & b"0000000000";
-        tmp(2) := rd & NOP & NOP & R03 & b"0000000000";
-        tmp(3) := cl & NOP & NOP & R03 & b"0000000000";
-        tmp(4) := je & R02 & R03 & NOP & b"0000000010";
-        tmp(5) := inc & R01 & NOP & R01 & b"0000000000";
-        tmp(6) := wr & R01 & NOP & NOP & b"0000000010";
-        tmp(7) := wr & R01 & NOP & NOP & b"0000000111";
-        tmp(8) := jmp & NOP & NOP & NOP & b"0000000010";
+        --      OPCODE  REGA  REGB  REGC    IMED/EndPerf
+        tmp(0)  := leaw & NOP & NOP & R01 & b"1111111000"; --carrega no R01 
+        tmp(1)  := wr   & R01 & NOP & NOP & b"0000111100"; --escreve na RAM 60
+        tmp(2)  := leaw & NOP & NOP & R05 & b"0000000000";
+        tmp(3)  := rd   & NOP & NOP & R06 & b"0000001000";
+        tmp(4)  := je   & R06 & R05 & NOP & b"0000000011";
+        tmp(5)  := leaw & NOP & NOP & R03 & b"0000001111"; --
+        tmp(6)  := rd   & NOP & NOP & R02 & b"0000001001";
+        tmp(7)  := je   & R02 & R03 & NOP & b"0000000110";
+        tmp(8)  := rd   & NOP & NOP & R04 & b"0000111100";
+        tmp(9)  := wr   & R04 & NOP & NOP & b"0000000111";
+        tmp(10) := leaw & NOP & NOP & R07 & b"0000000000";
+        tmp(11) := wr   & R07 & NOP & NOP & b"0000000001";
+        tmp(12) := inc  & R07 & NOP & R07 & b"0000000000";
+        
+        tmp(13) := wr   & R07 & NOP & NOP & b"0000000010";
+        tmp(14) := inc  & R07 & NOP & R07 & b"0000000000";
+        
+        tmp(15) := wr   & R07 & NOP & NOP & b"0000000011";
+        tmp(16) := inc  & R07 & NOP & R07 & b"0000000000";
+
+        tmp(17) := wr   & R07 & NOP & NOP & b"0000000100";
+        tmp(18) := inc  & R07 & NOP & R07 & b"0000000000";        
+
+        tmp(19) := wr   & R07 & NOP & NOP & b"0000000101";
+        tmp(20) := inc  & R07 & NOP & R07 & b"0000000000";        
+
+        tmp(21) := wr   & R07 & NOP & NOP & b"0000000110";
+        tmp(22) := inc  & R07 & NOP & R07 & b"0000000000";        
+
+        
+        
+        --tmp(0) := leaw & NOP & NOP & R01 & b"0000000011";
+        --tmp(1) := leaw & NOP & NOP & R02 & b"0000000000";
+        --tmp(2) := rd & NOP & NOP & R03 & b"0000000000";
+        --tmp(3) := cl & NOP & NOP & R03 & b"0000000000";
+        --tmp(4) := je & R02 & R03 & NOP & b"0000000010";
+        --tmp(5) := inc & R01 & NOP & R01 & b"0000000000";
+        --tmp(6) := wr & R01 & NOP & NOP & b"0000000010";
+        --tmp(7) := wr & R01 & NOP & NOP & b"0000000111";
+        --tmp(8) := jmp & NOP & NOP & NOP & b"0000000010";
         return tmp;
     end initMemory;
 
